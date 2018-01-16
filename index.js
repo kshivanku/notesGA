53db0a9ba42ccf51cfdd552d8759014d39b32476
kshivanku <kshivanku@gmail.com>
2018-01-16 15:17:23 -0500

    basic webhook

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ApiAiApp = require('actions-on-google').ApiAiApp;
+
+const restService = express();
+restService.use(bodyParser.json());
+
+restService.post('/hook', function(req, res){
+  console.log('hook request');
+  const app = new ApiAiApp({
+    request : req,
+    response : res
+  });
+
+  function welcomeIntent(app){
+    console.log("inside welcomeIntent");
+    app.ask("Hi this is welcome intent");
+  }
+})
+
+const actionMap = new Map();
+actionMap.set('input.welcome', welcomeIntent);
+
+restService.listen((process.env.PORT || 5000), function(){
+  console.log('server listening');
+})
