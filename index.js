859cb19f8a1e1d5633649b34af754e315e5db8c5
kshivanku <kshivanku@gmail.com>
2018-01-16 15:24:29 -0500

    stuff

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ restService.post('/hook', function(req, res){
     console.log("inside welcomeIntent");
     app.ask("Hi this is welcome intent");
   }
-})
 
-const actionMap = new Map();
-actionMap.set('input.welcome', welcomeIntent);
+  const actionMap = new Map();
+  actionMap.set('input.welcome', welcomeIntent);
 
-restService.listen((process.env.PORT || 5000), function(){
-  console.log('server listening');
+  restService.listen((process.env.PORT || 5000), function(){
+    console.log('server listening');
+  })
+
 })
