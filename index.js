90848235399a3f2424cbf4801bd12b6027d3cec9
kshivanku <kshivanku@gmail.com>
2018-01-16 18:54:45 -0500

    added the database stuff

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const DialogflowApp = require('actions-on-google').DialogflowApp;
 
+var databaseFile = "public/database/database.json";
+
 const restService = express();
 restService.use(bodyParser.json());
 restService.listen((process.env.PORT || 5000), function(){
@@ -21,18 +23,39 @@ restService.post('/hook', function(req, res){
   }
 
   function addNote(app) {
-    console.log(req.body.result.resolvedQuery);
-    app.tell("note added");
+    var raw_content = req.body.result.resolvedQuery;
+    var note_content = raw_content.split("add a note ")[1];
+    var database = JSON.parse(fs.readFileSync(databaseFile));
+    if (database.length == 0) {
+      database = new Array();
+    }
+    database.unshift(note_content);
+    fs.writeFileSync(databaseFile, JSON.stringify(database, null, 2));
+    app.ask('note added: ' + note_content);
   }
 
   function removeNote(app) {
     app.tell("note removed");
   }
 
+  function continueNote(app) {
+    app.tell("continue note");
+  }
+
+  function editNote(app) {
+    app.tell("edit note");
+  }
+
+  function repeatNote(app) {
+    app.tell("here is your last note");
+  }
+
   const actionMap = new Map();
   actionMap.set('input.welcome', welcomeIntent);
   actionMap.set('add.note', addNote);
   actionMap.set('remove.note', removeNote);
+  acrionMap.set('continue.note', continueNote);
+  acrionMap.set('repeat.note', repeatNote)
   app.handleRequest(actionMap);
 
 })
