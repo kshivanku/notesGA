13e7ee5c27d00a2d8ab56b4b5edf8a844368cc47
kshivanku <kshivanku@gmail.com>
2018-01-17 13:18:48 -0500

    front-end stuff

--- public/sketch.js
+++ public/sketch.js
@@ -3,7 +3,7 @@ var databaseFile = 'database/database.json';
 $(document).ready(function(){
   $.getJSON(databaseFile, function(data){
     for(var i = 0 ; i < data.length ; i++) {
-      $('#container').append('<p>' + data[i] + '</p>');
+      $('#notes_list').append('<p>' + data[i] + '</p>');
     }
   });
 });
