ae32e0245b9dc61b507095f8d5fffe93f791d52b
kshivanku <kshivanku@gmail.com>
2018-01-19 02:23:21 -0500

    ytdl-core

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const DialogflowApp = require('actions-on-google').DialogflowApp;
 const fs = require('fs');
 const moment = require('moment-timezone');
 const youtubedl = require('youtube-dl');
+const ytdl = require('ytdl-core');
+const path  = require('path');
+const https = require('https');
 
 var databaseFile = "public/database/database.json";
 var cameFromUnknown = false;
@@ -231,20 +234,31 @@ restService.post('/srtRequest', function(req, res) {
     //   console.log('subtitle files downloaded:', files);
     // });
 
-    var video = youtubedl('http://www.youtube.com/watch?v=90AiXO1pAiA',
-      // Optional arguments passed to youtube-dl.
-      ['--format=18'],
-      // Additional options can be given for calling `child_process.execFile()`.
-      { cwd: 'public/videos' });
+    ytdl.getInfo(url, (err, info) => {
+        if (err)
+            throw err;
+        var tracks = info.player_response.captions.playerCaptionsTracklistRenderer.captionTracks;
+        if (tracks && tracks.length) {
+            console.log('Found captions for', tracks.map(t => t.name.simpleText).join(', '));
+            var track = tracks.find(t => t.languageCode === lang);
+            if (track) {
+                console.log('Retrieving captions:', track.name.simpleText);
+                console.log('URL', track.baseUrl);
+                var output = `${info.title}.${track.languageCode}.xml`;
+                console.log('Saving to', output);
+                https.get(track.baseUrl, (res) => {
+                    res.pipe(fs.createWriteStream(path.resolve('public/videos', output)));
+                });
 
-    // Will be called when the download starts.
-    video.on('info', function(info) {
-      console.log('Download started');
-      console.log('filename: ' + info.filename);
-      console.log('size: ' + info.size);
+            } else {
+                console.log('Could not find captions for', lang);
+
+            }
+        } else {
+            console.log('No captions found for this video');
+        }
+
     });
-
-    video.pipe(fs.createWriteStream('myvideo.mp4'));
 
     responseData = {
         'txt': 'got your url ' + url
