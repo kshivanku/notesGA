952dfb57d596f301d69b299200cb60272ff5149f
kshivanku <kshivanku@gmail.com>
2018-01-20 18:33:05 -0500

    delete individual post

--- public/sketch.js
+++ public/sketch.js
@@ -1,24 +1,33 @@
 var databaseFile = 'database/database.json';
 
 $(document).ready(function(){
-  $.getJSON(databaseFile, function(data){
-    if(data.length > 0) {
-      $('#notes_list').empty();
-      for(var i = 0 ; i < data.length ; i++) {
-        if(data[i].source == 'Chrome Extension') {
-          $('#notes_list').append('<div class = "newEntry"><p class = "content">' + data[i].content + '</p><p class = "source">' + data[i].source + " | " + data[i].date + " | " + data[i].url + '</p></div>');
-        }
-        else {
-          $('#notes_list').append('<div class = "newEntry"><p class = "content">' + data[i].content + '</p><p class = "source">' + data[i].source + " | " + data[i].date + '</p></div>');
-        }
-      }
-    }
-  });
+  populateNotes();
   $('#deleteAllBtn').click(function(){
-    $('#notes_list').empty();
-    $('#notes_list').append('<div class="newEntry"><p class="content">No note yet</p><p class="source">No Source</p></div>');
+    // $('#notes_list').empty();
+    // $('#notes_list').append('<div class="newEntry"><p class="content">No note yet</p><p class="source">No Source</p></div>');
     $.get('/deleteAll', function(data){
-      console.log(data);
+      populateNotes();
     })
   })
+  $('body').on('click', '.delete_btn', function(){
+    console.log($(this));
+  })
+  function populateNotes(){
+    $.getJSON(databaseFile, function(data){
+      if(data.length > 0) {
+        $('#notes_list').empty();
+        for(var i = 0 ; i < data.length ; i++) {
+          if(data[i].source == 'Chrome Extension') {
+            $('#notes_list').append('<div class = "newEntry"><div class="text_area"><p class = "content">' + data[i].content + '</p><p class = "source">' + data[i].source + " | " + data[i].date + " | " + data[i].url + '</p></div><div id= "'+ data[i].content +'" class="delete_btn"></div></div>');
+          }
+          else if(data[i].source == 'Google Assistant') {
+            $('#notes_list').append('<div class = "newEntry"><div class="text_area"><p class = "content">' + data[i].content + '</p><p class = "source">' + data[i].source + " | " + data[i].date + '</p></div><div id= "'+ data[i].content +'" class="delete_btn"></div></div>');
+          }
+          else {
+            $('#notes_list').append('<div class = "newEntry"><div class="text_area"><p class = "content">' + data[i].content + '</p><p class = "source">' + data[i].source + " | " + data[i].date + '</p></div></div>');
+          }
+        }
+      }
+    });
+  }
 });
