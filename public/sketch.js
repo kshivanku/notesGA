0b017f16f0d0863355affd302628d15386d2c836
kshivanku <kshivanku@gmail.com>
2018-01-20 19:46:23 -0500

    individual url

--- public/sketch.js
+++ public/sketch.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
         $('#notes_list').empty();
         for(var i = 0 ; i < data.length ; i++) {
           if(data[i].source == 'Chrome Extension') {
-            $('#notes_list').append('<div class = "newEntry"><div class="text_area"><p class = "content">' + data[i].content + '</p><p class = "source">' + data[i].source + " | " + data[i].date + " | <a href = "+ data[i].url +" target ='_blank>'" + data[i].url + '</a></p></div><div id= "'+ data[i].content +'" class="delete_btn"></div></div>');
+            $('#notes_list').append('<div class = "newEntry"><div class="text_area"><p class = "content">' + data[i].content + '</p><p class = "source">' + data[i].source + ' | ' + data[i].date + ' | <a href="'+ data[i].url +'" target="_blank">' + data[i].url + '</a></p></div><div id = "' + data[i].content + '" class="delete_btn"></div></div>');
           }
           else if(data[i].source == 'Google Assistant') {
             $('#notes_list').append('<div class = "newEntry"><div class="text_area"><p class = "content">' + data[i].content + '</p><p class = "source">' + data[i].source + " | " + data[i].date + '</p></div><div id= "'+ data[i].content +'" class="delete_btn"></div></div>');
